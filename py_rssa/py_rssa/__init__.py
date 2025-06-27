cf9af139d34ed39090518055cc70da943426dde4
Alex Hoffmann <alexpaulhoffmann@gmail.com>
2025-06-27 17:34:15 -0400

    Add advanced SSA utilities

--- py_rssa/py_rssa/__init__.py
+++ py_rssa/py_rssa/__init__.py
@@ -1,7 +1,8 @@
 """Lightweight Python implementation of selected rssa routines."""
 
 from .ssa import SSA, ssa
+from .ossa import cond, pseudo_inverse, orthogonalize, svd_to_lrsvd, owcor
 from .hankel import hankel_mv
 from . import datasets
 
-__all__ = ["SSA", "ssa", "datasets", "hankel_mv"]
+__all__ = ["SSA", "ssa", "datasets", "hankel_mv", "cond", "pseudo_inverse", "orthogonalize", "svd_to_lrsvd", "owcor"]
