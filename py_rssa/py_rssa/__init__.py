b630d2ffda30dac7691aa5ad896c6c05b2f27f89
Alex Hoffmann <alexpaulhoffmann@gmail.com>
2025-06-27 17:25:29 -0400

    Add Python utilities mirroring R common helpers

--- py_rssa/py_rssa/__init__.py
+++ py_rssa/py_rssa/__init__.py
@@ -3,5 +3,12 @@
 from .ssa import SSA, ssa
 from .hankel import hankel_mv
 from . import datasets
+from . import utils
 
-__all__ = ["SSA", "ssa", "datasets", "hankel_mv"]
+__all__ = [
+    "SSA",
+    "ssa",
+    "datasets",
+    "hankel_mv",
+    "utils",
+]
