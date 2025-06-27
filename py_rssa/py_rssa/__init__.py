ccf50b07128c82e6630aef33d7b8f499c06d9e38
Alex Hoffmann <alexpaulhoffmann@gmail.com>
2025-06-27 17:23:40 -0400

    Add complex Hankel helpers

--- py_rssa/py_rssa/__init__.py
+++ py_rssa/py_rssa/__init__.py
@@ -1,7 +1,9 @@
 """Lightweight Python implementation of selected rssa routines."""
 
-from .ssa import SSA, ssa
+from .ssa import SSA, ssa, hankel_weights
 from .hankel import hankel_mv
+from .chankel import chankel_mv, chankelize, chankelize_multi
 from . import datasets
 
-__all__ = ["SSA", "ssa", "datasets", "hankel_mv"]
+__all__ = ["SSA", "ssa", "datasets", "hankel_mv", "chankel_mv",
+           "chankelize", "chankelize_multi", "hankel_weights"]
