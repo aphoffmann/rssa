203b519446a982cb9a818e456304e6cfd553c365
Alex Hoffmann <alexpaulhoffmann@gmail.com>
2025-06-27 17:34:19 -0400

    Add matplotlib plotting helpers

--- py_rssa/py_rssa/__init__.py
+++ py_rssa/py_rssa/__init__.py
@@ -2,6 +2,23 @@
 
 from .ssa import SSA, ssa
 from .hankel import hankel_mv
+from .plot import (
+    plot,
+    plot_values,
+    plot_vectors,
+    plot_series,
+    plot_wcor,
+)
 from . import datasets
 
-__all__ = ["SSA", "ssa", "datasets", "hankel_mv"]
+__all__ = [
+    "SSA",
+    "ssa",
+    "datasets",
+    "hankel_mv",
+    "plot",
+    "plot_values",
+    "plot_vectors",
+    "plot_series",
+    "plot_wcor",
+]
