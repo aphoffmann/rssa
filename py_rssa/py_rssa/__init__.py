55a7aabaf2f0a703f212c18d347eb298b5000422
Alex Hoffmann <alexpaulhoffmann@gmail.com>
2025-06-27 17:21:34 -0400

    Add Python capability checking

--- py_rssa/py_rssa/__init__.py
+++ py_rssa/py_rssa/__init__.py
@@ -3,5 +3,20 @@
 from .ssa import SSA, ssa
 from .hankel import hankel_mv
 from . import datasets
+from .capabilities import (
+    register_capability,
+    capable,
+    object_capabilities,
+    fftw_available,
+)
 
-__all__ = ["SSA", "ssa", "datasets", "hankel_mv"]
+__all__ = [
+    "SSA",
+    "ssa",
+    "datasets",
+    "hankel_mv",
+    "register_capability",
+    "capable",
+    "object_capabilities",
+    "fftw_available",
+]
