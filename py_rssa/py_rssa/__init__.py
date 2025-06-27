c04f0ec8eafd7f83c1a87de95d895041b410830b
Alex Hoffmann <alexpaulhoffmann@gmail.com>
2025-06-27 17:34:13 -0400

    Add basic MSSA support

--- py_rssa/py_rssa/__init__.py
+++ py_rssa/py_rssa/__init__.py
@@ -1,7 +1,8 @@
 """Lightweight Python implementation of selected rssa routines."""
 
 from .ssa import SSA, ssa
+from .mssa import MSSA, mssa, reconstruct, gapfill, forecast
 from .hankel import hankel_mv
 from . import datasets
 
-__all__ = ["SSA", "ssa", "datasets", "hankel_mv"]
+__all__ = ["SSA", "ssa", "MSSA", "mssa", "reconstruct", "gapfill", "forecast", "datasets", "hankel_mv"]
