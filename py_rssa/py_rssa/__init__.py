36a73a90e0d6451ba69b4b0cacd9d48193bab45f
Alex Hoffmann <alexpaulhoffmann@gmail.com>
2025-06-27 17:34:17 -0400

    Add parameter estimation utilities

--- py_rssa/py_rssa/__init__.py
+++ py_rssa/py_rssa/__init__.py
@@ -1,7 +1,22 @@
 """Lightweight Python implementation of selected rssa routines."""
 
 from .ssa import SSA, ssa
+from .parestimate import (
+    roots2pars,
+    parestimate_pairs,
+    parestimate_esprit,
+    parestimate,
+)
 from .hankel import hankel_mv
 from . import datasets
 
-__all__ = ["SSA", "ssa", "datasets", "hankel_mv"]
+__all__ = [
+    "SSA",
+    "ssa",
+    "datasets",
+    "hankel_mv",
+    "roots2pars",
+    "parestimate_pairs",
+    "parestimate_esprit",
+    "parestimate",
+]
