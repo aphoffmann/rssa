4e301a1e99ffd8d49ca0338202a9c8d07ab71e1f
Alex Hoffmann <alexpaulhoffmann@gmail.com>
2025-06-27 17:32:09 -0400

    Add heterogeneity matrix helper

--- py_rssa/py_rssa/__init__.py
+++ py_rssa/py_rssa/__init__.py
@@ -2,6 +2,14 @@
 
 from .ssa import SSA, ssa
 from .hankel import hankel_mv
+from .hmatr import hmatr, plot_hmatr
 from . import datasets
 
-__all__ = ["SSA", "ssa", "datasets", "hankel_mv"]
+__all__ = [
+    "SSA",
+    "ssa",
+    "datasets",
+    "hankel_mv",
+    "hmatr",
+    "plot_hmatr",
+]
