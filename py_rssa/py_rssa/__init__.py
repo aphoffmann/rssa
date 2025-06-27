90bfab0fbf6ae63611d45133dec380dee8001950
Alex Hoffmann <alexpaulhoffmann@gmail.com>
2025-06-27 17:34:01 -0400

    Add block-Hankel helper utilities

--- py_rssa/py_rssa/__init__.py
+++ py_rssa/py_rssa/__init__.py
@@ -2,6 +2,25 @@
 
 from .ssa import SSA, ssa
 from .hankel import hankel_mv
+from .hbhankel import (
+    convolution_dims,
+    convolven,
+    factor_mask_2d,
+    field_weights_2d,
+    ball_mask,
+    simplex_mask,
+)
 from . import datasets
 
-__all__ = ["SSA", "ssa", "datasets", "hankel_mv"]
+__all__ = [
+    "SSA",
+    "ssa",
+    "datasets",
+    "hankel_mv",
+    "convolution_dims",
+    "convolven",
+    "factor_mask_2d",
+    "field_weights_2d",
+    "ball_mask",
+    "simplex_mask",
+]
