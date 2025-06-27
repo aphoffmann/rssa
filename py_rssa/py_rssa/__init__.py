dc2e15680ba1265f38ce94e8a33b1d7fb86f3cfe
Alex Hoffmann <alexpaulhoffmann@gmail.com>
2025-06-27 17:34:23 -0400

    Add Python multi-series plotting utilities

--- py_rssa/py_rssa/__init__.py
+++ py_rssa/py_rssa/__init__.py
@@ -2,6 +2,21 @@
 
 from .ssa import SSA, ssa
 from .hankel import hankel_mv
+from .plotn import (
+    do_slice_array,
+    do_slice_reconstruction,
+    plot_nd_reconstruction,
+    plot_ssa_vectors_nd,
+)
 from . import datasets
 
-__all__ = ["SSA", "ssa", "datasets", "hankel_mv"]
+__all__ = [
+    "SSA",
+    "ssa",
+    "datasets",
+    "hankel_mv",
+    "do_slice_array",
+    "do_slice_reconstruction",
+    "plot_nd_reconstruction",
+    "plot_ssa_vectors_nd",
+]
